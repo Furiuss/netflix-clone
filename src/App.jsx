c3ab1f4459a999959b3d6b06d1154678b579c2cc
André <andremessias.az@gmail.com>
2022-07-20 21:15:07 -0300

    finish tmdb file and setup app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Tmdb from './Tmdb'
 import './App.css'
 
 function App() {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList()
+      setMovieList(list)
+    }
+
+    loadAll()
+  }, [])
+
   return (
     <div className="App">
-      
+      {movieList.map((movie) => (
+        <p>{movie.slug}</p>
+      ))}
     </div>
   )
 }
--- src/Tmdb.js
+++ src/Tmdb.js
@@ -17,33 +17,38 @@ export default{
       },
       {
         slug: 'trending',
-        title: 'Em Alta',
-        items: []
+        title: 'Recomendamos para você',
+        items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'toprated',
+        title: 'Em alta',
+        items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
       },
       {
         slug: 'action',
         title: 'Ação',
-        items: []
+        items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
       },
       {
         slug: 'comedy',
         title: 'Comédia',
-        items: []
+        items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
       },
       {
         slug: 'horror',
         title: 'Terror',
-        items: []
+        items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
       },
       {
         slug: 'romance',
         title: 'Romance',
-        items: []
+        items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
       },
       {
         slug: 'documentary',
         title: 'Documentários',
-        items: []
+        items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
       }
     ]
   }
