92d0992edc16fcd223ccca99eb16fc70417e3398
André <andremessias.az@gmail.com>
2022-07-23 18:48:18 -0300

    header style almost done

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,16 @@ import './syle.css'
 export const Header = () => {
   return (
     <header>
-      
+      <div className="header--logo">
+        <a href="/">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="" />
+        </a>
+      </div>
+      <div className="header--user">
+        <a href="/">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPsVAeFlYeYOEUzb3TV1ML91_LPkkFML5lRQcMdr9nQu2CqO-WzT-RLmkM5_cOKvkaBkI&usqp=CAU" alt="" />
+        </a>
+      </div>
     </header>
   )
 }
