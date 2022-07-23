da8bf1e849fcd715add6260646018f57d4789407
André <andremessias.az@gmail.com>
2022-07-22 22:22:29 -0300

    add linear gradient in featured background to make an smooth effect

--- src/components/FeatureMovie/index.jsx
+++ src/components/FeatureMovie/index.jsx
@@ -10,6 +10,9 @@ export const FeatureMovie = ({item}) => {
           backgroundPosition: 'center',
           backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
         }}>
+          <div className="featured--vertical">
+            
+          </div>
           <div>{item.original_name}</div>
         </section>
       }
