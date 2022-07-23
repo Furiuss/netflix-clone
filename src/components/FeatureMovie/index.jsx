a1208f1f8d1d67dc8c522cfceff7d1a5fecdedf3
André <andremessias.az@gmail.com>
2022-07-22 23:08:15 -0300

    featured seasons  successfully refactored

--- src/components/FeatureMovie/index.jsx
+++ src/components/FeatureMovie/index.jsx
@@ -17,9 +17,7 @@ export const FeatureMovie = ({item}) => {
                 <div className="featured--points">{item.vote_average} pontos</div>
                 <div className="featured--year">2099</div>
                 <div className="featured--seasons"> 
-                { item.number_of_seasons > 1 
-                  ? <p>{item.number_of_seasons} temporadas</p> 
-                  : <p>{item.number_of_seasons} temporada</p> }
+                  {item.number_of_seasons} temporada{item.number_of_seasons > 1 ? 's' : ''}
                 </div>
               </div>
             </div>
