94acde8db66f6abd04afea1400116837783ce252
André <andremessias.az@gmail.com>
2022-07-22 22:27:37 -0300

    setup now horizontal smooth effect in fetured horizontal

--- src/components/FeatureMovie/index.jsx
+++ src/components/FeatureMovie/index.jsx
@@ -11,7 +11,7 @@ export const FeatureMovie = ({item}) => {
           backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
         }}>
           <div className="featured--vertical">
-            
+            <div className="featured--horizontal"></div>
           </div>
           <div>{item.original_name}</div>
         </section>
