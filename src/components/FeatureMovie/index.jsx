a280c8797dd38c76b76cdad7c6c5a5303bca2cd2
André <andremessias.az@gmail.com>
2022-07-22 22:37:10 -0300

    setup featured infos. I need to refact featured seasons

--- src/components/FeatureMovie/index.jsx
+++ src/components/FeatureMovie/index.jsx
@@ -11,7 +11,18 @@ export const FeatureMovie = ({item}) => {
           backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
         }}>
           <div className="featured--vertical">
-            <div className="featured--horizontal"></div>
+            <div className="featured--horizontal">
+              <div className="feature--name">{item.original_name}</div>
+              <div className="featured--info">
+                <div className="featured--points">{item.vote_average} pontos</div>
+                <div className="featured--year">2099</div>
+                <div className="featured--seasons"> 
+                  {item.number_of_seasons > 1 
+                  ? <p>{item.number_of_seasons} temporadas</p> 
+                  : <p>{item.number_of_seasons} temporada</p>}
+                </div>
+              </div>
+            </div>
           </div>
           <div>{item.original_name}</div>
         </section>
