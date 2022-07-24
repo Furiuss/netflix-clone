5a422cec2d14014e4c53df3b2c55d57236442fcc
André <andremessias.az@gmail.com>
2022-07-23 22:53:58 -0300

    fix header dinamic classname

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import './syle.css'
 
 export const Header = ({black}) => {
   return (
-    <header className={black && "black"}>
+    <header className={black ? "black" : ""}>
       <div className="header--logo">
         <a href="/">
           <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="" />
