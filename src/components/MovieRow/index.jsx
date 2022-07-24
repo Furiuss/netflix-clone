7e9285997fffd66cad49688c9f189d54309775e1
André <andremessias.az@gmail.com>
2022-07-23 23:10:55 -0300

    solved. Fix next slide bug

--- src/components/MovieRow/index.jsx
+++ src/components/MovieRow/index.jsx
@@ -18,14 +18,12 @@ export const MovieRow = ({title, items}) => {
   }
 
   const handleRightArrow = () => {
-    const windowWidth = Math.round(window.innerWidth / 2);
-        let x = scrollX - windowWidth;
-        const max = (-1 * items.results.length * 250) + (2 * windowWidth) - 60;
-        if( x < max ){
-            x = max;
-        }
-
-        setScrollX(x);
+    let x = scrollX - Math.round(window.innerWidth / 2)
+    let listW = items.results.length * 150;
+     if((window.innerWidth - listW) > x) {
+      x = (window.innerWidth - listW) - 60
+     }
+     setScrollX(x);
   }
 
   return (
@@ -39,7 +37,6 @@ export const MovieRow = ({title, items}) => {
       <div className="movieRow--right" onClick={handleRightArrow}>
         <NavigateNextIcon style={{fontSize: 50}} />
       </div>
-      
       <div className="movieRow--listarea">
         <div className="movieRow--list" style={{
               marginLeft: scrollX,
