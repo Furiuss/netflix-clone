147f66a45a6f18b809f4bd0722485c8ad29bc539
André <andremessias.az@gmail.com>
2022-07-24 22:49:11 -0300

    some changes in navbar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React from "react";
 
-import './syle.css'
+import "./syle.css";
 
-export const Header = ({black}) => {
+export const Header = ({ black }) => {
   return (
     <header className={black ? "black" : ""}>
-      <div className="header--logo">
-        <a href="/">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="" />
-        </a>
+      <div className="header--left">
+        <div className="header--logo">
+          <a href="/">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+              alt=""
+            />
+          </a>
+        </div>
+
+        
+        
       </div>
-      <div className="header--user">
-        <a href="/">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPsVAeFlYeYOEUzb3TV1ML91_LPkkFML5lRQcMdr9nQu2CqO-WzT-RLmkM5_cOKvkaBkI&usqp=CAU" alt="" />
-        </a>
+
+      <div className="header--right">
+        <div className="header--user">
+          <a href="/">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPsVAeFlYeYOEUzb3TV1ML91_LPkkFML5lRQcMdr9nQu2CqO-WzT-RLmkM5_cOKvkaBkI&usqp=CAU"
+              alt=""
+            />
+          </a>
+        </div>
       </div>
     </header>
-  )
-}
+  );
+};
