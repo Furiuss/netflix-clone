5391b6170d30bbac2b4f5514835cda64b75e56de
André <andremessias.az@gmail.com>
2022-07-24 22:02:23 -0300

    add others pages and react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+import App from "./App";
+import Home from "./pages/Home";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route element={<App />}>
+        <Route path="/" element={<Home />} />
+        <Route path="movie/:id" element={<Movie />} />
+        <Route path="search" element={<Search />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
